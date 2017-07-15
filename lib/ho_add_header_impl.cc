a15760c899f9efe3c2f204ded8f66f20486efee3
Leonard Göhrs <leonard@goehrs.eu>
2017-07-15 12:08:15 +0200

    Header adder seems to be working

--- lib/ho_add_header_impl.cc
+++ lib/ho_add_header_impl.cc
@@ -1,17 +1,17 @@
 /* -*- c++ -*- */
-/* 
- * Copyright 2017 <+YOU OR YOUR COMPANY+>.
- * 
+/*
+ * Copyright 2017 Leonard Göhrs <leonard@goehrs.eu>.
+ *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3, or (at your option)
  * any later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this software; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street,
@@ -23,6 +23,7 @@
 #endif
 
 #include <gnuradio/io_signature.h>
+#include <gnuradio/digital/crc32.h>
 #include "ho_add_header_impl.h"
 
 namespace gr {
@@ -40,9 +41,11 @@ namespace gr {
      */
     ho_add_header_impl::ho_add_header_impl(const std::string& len_tag_key)
       : gr::tagged_stream_block("ho_add_header",
-              gr::io_signature::make(<+MIN_IN+>, <+MAX_IN+>, sizeof(<+ITYPE+>)),
-              gr::io_signature::make(<+MIN_OUT+>, <+MAX_OUT+>, sizeof(<+OTYPE+>)), <+len_tag_key+>)
-    {}
+                                gr::io_signature::make(1, 1, sizeof(uint8_t)),
+                                gr::io_signature::make(1, 1, sizeof(uint8_t)),
+                                len_tag_key)
+    {
+    }
 
     /*
      * Our virtual destructor.
@@ -54,20 +57,33 @@ namespace gr {
     int
     ho_add_header_impl::calculate_output_stream_length(const gr_vector_int &ninput_items)
     {
-      int noutput_items = /* <+set this+> */;
+      int noutput_items = ninput_items[0] + 8;
       return noutput_items ;
     }
 
     int
     ho_add_header_impl::work (int noutput_items,
-                       gr_vector_int &ninput_items,
-                       gr_vector_const_void_star &input_items,
-                       gr_vector_void_star &output_items)
+                              gr_vector_int &ninput_items,
+                              gr_vector_const_void_star &input_items,
+                              gr_vector_void_star &output_items)
     {
-      const <+ITYPE+> *in = (const <+ITYPE+> *) input_items[0];
-      <+OTYPE+> *out = (<+OTYPE+> *) output_items[0];
+      const uint8_t *in = (const uint8_t *) input_items[0];
+      uint8_t *out = (uint8_t *) output_items[0];
 
-      // Do <+signal processing+>
+      uint32_t payload_len= ninput_items[0];
+      uint32_t crc= gr::digital::crc32(in, payload_len);
+
+      out[0]= (payload_len >> 24) & 0xff;
+      out[1]= (payload_len >> 16) & 0xff;
+      out[2]= (payload_len >>  8) & 0xff;
+      out[3]= (payload_len >>  0) & 0xff;
+
+      out[4]= (crc >> 24) & 0xff;
+      out[5]= (crc >> 16) & 0xff;
+      out[6]= (crc >>  8) & 0xff;
+      out[7]= (crc >>  0) & 0xff;
+
+      memcpy(&out[8], in, payload_len);
 
       // Tell runtime system how many output items we produced.
       return noutput_items;
@@ -75,4 +91,3 @@ namespace gr {
 
   } /* namespace hnez_ofdm */
 } /* namespace gr */
-
--- python/qa_ho_add_header.py
+++ python/qa_ho_add_header.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-# 
-# Copyright 2017 <+YOU OR YOUR COMPANY+>.
-# 
+#
+# Copyright 2017 Leonard Göhrs <leonard@goehrs.eu>.
+#
 # This is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3, or (at your option)
 # any later version.
-# 
+#
 # This software is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this software; see the file COPYING.  If not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street,
 # Boston, MA 02110-1301, USA.
-# 
+#
 
 from gnuradio import gr, gr_unittest
 from gnuradio import blocks
@@ -32,10 +32,26 @@ class qa_ho_add_header (gr_unittest.TestCase):
         self.tb = None
 
     def test_001_t (self):
-        # set up fg
-        self.tb.run ()
-        # check data
+        data= (1, 2, 3, 4, 5, 6, 7, 8)
 
+        data_src= blocks.vector_source_b(data, False, 1, [])
+        stream_tagger= blocks.stream_to_tagged_stream(gr.sizeof_char, 1, len(data), "packet_len")
+        header_adder= hnez_ofdm.ho_add_header("packet_len")
+        dst= blocks.vector_sink_b()
+
+        self.tb.connect((data_src, 0), (stream_tagger, 0))
+        self.tb.connect((stream_tagger, 0), (header_adder, 0))
+        self.tb.connect((header_adder, 0), (dst, 0))
+
+        self.tb.run ()
+
+        expected_result= (0, 0, 0, 8, 235, 244, 114, 39) + data
+        actual_result= dst.data()
+
+        self.assertSequenceEqual(
+            expected_result,
+            actual_result[:len(expected_result)]
+        )
 
 if __name__ == '__main__':
     gr_unittest.run(qa_ho_add_header, "qa_ho_add_header.xml")
